86229411fb0af47cce008e9a611b178f905eb546
analializardi884-hub <analia.lizardi884@students.cmcss.net>
2026-02-17 08:55:35 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -9,102 +9,102 @@
 
 # --- Snippet 1 ---
 # This should display a greeting
-message = "Hello, welcome to Python!
+message = ("Hello, welcome to Python! ")
 print(message)
 
 # --- Snippet 2 ---
 # This should display a student's name
-student_name = "Jordan"
-print(f"The student is {studentname}")
+student_name = ("Jordan")
+print(f"The student is {student_name}")
 
 # --- Snippet 3 ---
 # This should display a favorite color
-favorite_color = "blue"
-print (f"My favorite color is {favorite_color}")
+favorite_color = ("blue")
+print(f"My favorite color is {favorite_color}")
 
 # --- Snippet 4 ---
 # This should display a city name
-city = Austin
+city = ("Austin")
 print(f"I live in {city}")
 
 # --- Snippet 5 ---
 # This should display the number of pets
-number of pets = 5
+number_of_pets = 5
 print(f"I have {number_of_pets} pets")
 
 # --- Snippet 6 ---
 # This should display a book title
-book_title = "The Great Gatsby'
+book_title = ("The Great Gatsby")
 print(f"I am reading {book_title}")
 
 # --- Snippet 7 ---
 # This should display a greeting
-greeting = "Good morning!"
-  print(f"{greeting}")
+greeting = ("Good morning!")
+print(f"{greeting}")
 
 # --- Snippet 8 ---
 # This should display a temperature
 temperature = 72
-print(f"The temperature is {temperture} degrees")
+print(f"The temperature is {temperature} degrees")
 
 # --- Snippet 9 ---
 # This should display a team name
-team_name = "Eagles"
-print f"Go {team_name}!"
+team_name = ("Eagles")
+print(f"Go {team_name}!")
 
 # --- Snippet 10 ---
 # This should display a food item
-favorite_food = "pizza"
+favorite_food = ("pizza")
 print(f"I love (favorite_food}")
 
 # --- Snippet 11 ---
 # This should display an animal
-animal = "dog"
+animal = ("dog")
 print(f"My favorite animal is {animal}')
 
 # --- Snippet 12 ---
 # This should display a school name
-school name = "Central High"
+school_name = ("Central High")
 print(f"I attend {school_name}")
 
 # --- Snippet 13 ---
 # This should display a movie title
-movie_title = Inception
+movie_title = ("Inception")
 print(f"My favorite movie is {movie_title}")
 
 # --- Snippet 14 ---
 # This should display a fruit
-fruit = "apple"
-print(f"I like to eat {frut}")
+fruit = ("apple")
+print(f"I like to eat {fruit}")
 
 # --- Snippet 15 ---
 # This should display a sport
-favorite_sport = "basketball"
+favorite_sport = ("basketball")
    print(f"I play {favorite_sport}")
 
 # --- Snippet 16 ---
 # This should display a greeting
-morning_greeting = "Rise and shine!"
-print(f{morning_greeting})
+morning_greeting = ("Rise and shine!")
+print(f"{morning_greeting}")
 
 # --- Snippet 17 ---
 # This should display a car color
-car_color = "red"
+car_color = ("red")
 print(f"My car is {car_color})
 
 # --- Snippet 18 ---
 # This should display a day
-3rd_day = "Wednesday"
+3rd_day = ("Wednesday")
 print(f"Today is {3rd_day}")
 
 # --- Snippet 19 ---
 # This should display a state
-home_state = "Texas"
+home_state = ("Texas")
 prnt(f"I live in {home_state}")
 
 # --- Snippet 20 ---
 # This should display a season
-favorite_season = 'summer"
+favorite_season = ("summer")
 print(f"My favorite season is {favorite_season}")
 
 # --- Snippet 21 ---
